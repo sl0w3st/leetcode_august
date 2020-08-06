cc02e4f9c1b228a2c3253b3585244725d9e8099a
slowest <soslow4u@gmail.com>
2020-08-07 07:26:53 +0900

    w1p2

---
+++ august-week1-problem2.js
@@ -0,0 +1,6 @@
+/**
+https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/549/week-1-august-1st-august-7th/3409/
+ */
+var detectCapitalUse = function(word = "") {
+    return /[A-Z]+$|[A-Z][a-z]+$/.test(word);
+};
